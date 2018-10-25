29c977de2e3cbf94eda0aa645e470da07be43160
SzilvasiPeter <peti.szilvasi95@gmail.com>
2018-10-25 22:09:47 +0200

    Created a blog page

--- app/views.py
+++ app/views.py
@@ -1,8 +1,34 @@
 from app import app
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+
+posts = [
+	{
+		'author': 'Szilvasi Peter',
+		'title': 'Blog Post1',
+		'content': 'First post content',
+		'date_posted': '2018.10.25'
+	},
+	{
+		'author': 'John Doe',
+		'title': 'Blog Post 2',
+		'content': 'Second post content',
+		'date_posted': '2018.10.26'
+	}
+]
 
 @app.route('/')
 @app.route('/index')
 def index():
 	return render_template("main.html")
 
+@app.route('/dashboard/')
+def dashboard():
+	return render_template("dashboard.html")
+
+@app.route('/blog/')
+def blog():
+	return render_template("blog.html", posts=posts, title="Blog")
+
+@app.route('/blog/about/')
+def blog_about():
+	return render_template("blogAbout.html", title="About")

a4d830731d054a8dc02ad17ebbb55a146488efab
SzilvasiPeter <peti.szilvasi95@gmail.com>
2018-10-28 18:57:44 +0100

    Add Login Modal to main page, Make forms.py script for handling login and registration

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from app import app
 from flask import Flask, render_template, url_for
+# from app.forms import RegistrartionForm, LoginForm
+
+app.config['SECRET_KEY'] = '63483d93977177f94771262462df57fb'
 
 posts = [
 	{
@@ -32,3 +35,13 @@ def blog():
 @app.route('/blog/about/')
 def blog_about():
 	return render_template("blogAbout.html", title="About")
+
+# @app.route('/register/')
+# def register():
+# 	form = RegistrartionForm()
+# 	return render_template('register.html', title='Register', form=form)
+
+# @app.route('/login/')
+# def login():
+# 	form = LoginForm()
+# 	return render_template('login.html', title='Login', form=form)

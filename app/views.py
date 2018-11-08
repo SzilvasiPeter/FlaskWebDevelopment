a32fec24624c2e345443f73e119228e51db35e05
SzilvasiPeter <peti.szilvasi95@gmail.com>
2018-11-08 23:13:31 +0100

    Make fake login (not tested), Need to link controller-view, Need database for use real data

--- app/views.py
+++ app/views.py
@@ -47,4 +47,10 @@ def register():
 @app.route('/login/')
 def login():
 	form = LoginForm()
+	if form.validate_on_submit():
+		if form.email.data == 'admin@gmail.com' and form.password.data == 'password':
+			flash('You have been logged in!', 'success')
+			return redirect(url_for('index'))
+		else:
+			flash('Login Unsuccessfull. Please check username and password', 'danger')
 	return render_template('login.html', title='Login', form=form)

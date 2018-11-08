aaa29ef8d50aaa11f3690a4758e8029f1ee9c61b
SzilvasiPeter <peti.szilvasi95@gmail.com>
2018-11-08 23:23:24 +0100

    Refactor id to class

--- app/static/js/scrolling-nav.js
+++ app/static/js/scrolling-nav.js
@@ -3,14 +3,14 @@
 
   $(window).scroll(function() {
     if($(window).scrollTop() > 80){
-      $('#mainNav').css({'padding' : '0px'});
-      $('#myLogo').css({'height': '60px', 'width' : '60px', 'margin-left' : '5px'});
-      $('#nav-link').css({'margin' : '8px'});
-      $('#LoginButton').css({'margin-right' : '8px'});
+      $('.mainNav').css({'padding' : '0px'});
+      $('.myLogo').css({'height': '60px', 'width' : '60px', 'margin-left' : '5px'});
+      $('.nav-link').css({'margin' : '8px'});
+      $('.LoginButton').css({'margin-right' : '8px'});
      }else{
-      $('#mainNav').css({'padding' : '28px'});
-      $('#myLogo').css({'height': '80px', 'width' : '80px'});
-      $('#nav-link').css({'margin' : '15px'});
+      $('.mainNav').css({'padding' : '28px'});
+      $('.myLogo').css({'height': '80px', 'width' : '80px', 'margin-left' : '10px'});
+      $('.nav-link').css({'margin' : '15px'});
      }
    });
 

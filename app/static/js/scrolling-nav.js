7fcb6721d2b7a64ce20506e4d768b29cbccb9537
SzilvasiPeter <peti.szilvasi95@gmail.com>
2018-11-18 13:59:22 +0100

    Add database with Flask-SQLAlchemy. Database two table: User and Posts

--- app/static/js/scrolling-nav.js
+++ app/static/js/scrolling-nav.js
@@ -1,6 +1,7 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  // Scrolling navigation bar
   $(window).scroll(function() {
     if($(window).scrollTop() > 80){
       $('.mainNav').css({'padding' : '0px'});
@@ -13,5 +14,30 @@
       $('.nav-link').css({'margin' : '15px'});
      }
    });
+
+  // var email = $('#email').val();
+  // var password = $('#password').val();
+
+  // // Don't close modal if invalid email or password
+  // $('.submitAction').click(function() {
+  //   if( email == "" || password == "" || password.length < 8){
+  //       //$('#showError').show();
+  //       return false;
+  //   }
+  //   else{
+  //     return true;
+  //   }
+  // });
+
+// // Function for check email format 
+// function isEmail(email) {
+//   var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+//   if (regex.test(email)){
+//     return true;
+//   }
+//   else{
+//     return false;
+//   }
+// }
 
 })(jQuery); // End of use strict
--- app/views.py
+++ app/views.py
@@ -1,8 +1,36 @@
-from app import app
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
-from app.forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
+# from validate_email import validate_email
+import re
+import sys
+app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '63483d93977177f94771262462df57fb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(20), unique=True, nullable=False)
+	email = db.Column(db.String(120), unique=True, nullable=False)
+	image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+	password = db.Column(db.String(60), nullable=False)
+	posts = db.relationship('Post', backref='author', lazy=True)
+
+	def __repr__(self):
+		return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(100), nullable=False)
+	date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+	content = db.Column(db.Text, nullable=False)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+	def __repr__(self):
+		return f"Post('{self.title}', '{self.date_posted}')"
 
 posts = [
 	{
@@ -23,15 +51,13 @@ posts = [
 @app.route('/index', methods=['GET', 'POST'])
 def index():
 	form1 = LoginForm()
-	displayModal = False
 	if form1.validate_on_submit():
-		if form1.email.data == 'admin@gmail.com' and form1.password.data == 'password':
+		if (form1.email.data == 'admin@gmail.com' and is_email_valid(form1.email.data) and form1.password.data == 'password'):
 			flash('You have been logged in!', 'success')
-			return render_template('mainPage.html', form1=form1, displayModal=displayModal)
+			return render_template('mainPage.html', form1=form1)
 		else:
 			flash('Login Unsuccessfull. Please check username and password', 'danger')
-			displayModal = True
-			return render_template('mainPage.html', form1=form1, displayModal=displayModal)
+			return render_template('mainPage.html', form1=form1)
 	return render_template('mainPage.html', form1=form1)
 	# return render_template("mainPage.html")
 
@@ -54,3 +80,10 @@ def register():
 		flash(f'Account created for {form.username.data}!', 'success')
 		return redirect(url_for('index'))
 	return render_template('register.html', title='Register', form=form)
+
+
+def is_email_valid(email):
+    """Validate the email address using a regex."""
+    if not re.match("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$", email):
+        return False
+    return True

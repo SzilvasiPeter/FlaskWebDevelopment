0e30b7c4f3e40952f0c4fd16fb07f8948e1fe1e2
SzilvasiPeter <peti.szilvasi95@gmail.com>
2018-11-27 19:40:21 +0100

    Rewrite some code, so we can go to the blog route without sign in, Create login route in blog layout

--- app/views.py
+++ app/views.py
@@ -51,6 +51,21 @@ def register():
 		return redirect(url_for('index'))
 	return render_template('register.html', title='Register', form=form)
 
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		user  = User.query.filter_by(email=form.email.data).first()
+		if user and bcrypt.check_password_hash(user.password, form.password.data):
+			hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+			flash('Login is successfull.', 'success')
+			login_user(user, remember=form.remember.data)
+			next_page = request.args.get('next')
+			return redirect(next_page) if next_page else redirect(url_for('blog'))
+		else:
+			flash('Login Unsuccessfull. Your email or password is wrong', 'danger')
+	return render_template('login.html', title='Login', form=form)
+
 @app.route('/logout')
 def logout():
 	logout_user()

8e8eb720f05bfa63764c0df2501a9448d7b0ce3b
SzilvasiPeter <peti.szilvasi95@gmail.com>
2018-12-01 13:30:39 +0100

    Add reseting password for users, flask webapp send reset email (only gmail)

--- app/forms.py
+++ app/forms.py
@@ -28,7 +28,7 @@ class RegistrationForm(FlaskForm):
 	def validate_email(self, email):
 		user = User.query.filter_by(email=email.data).first()
 		if user:
-			raise ValidationError('The	 Email is already exist. Please take a different one.')
+			raise ValidationError('The Email is already exist. Please take a different one.')
 
 class LoginForm(FlaskForm):
 	email = StringField('Email', 
@@ -67,3 +67,22 @@ class PostForm(FlaskForm):
 	title = StringField('Title', validators=[DataRequired()])
 	content = TextAreaField('Content', validators=[DataRequired()])
 	submit = SubmitField('Post')
+
+
+class RequestResetForm(FlaskForm):
+	email = StringField('Email', 
+		validators=[DataRequired(), Email()])
+	submit = SubmitField('Request Password Reset')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user is None:
+			raise ValidationError('There is no account with that email. You must register first.')
+
+class ResetPasswordForm(FlaskForm):
+	password = PasswordField('Password',
+		validators=[DataRequired(), Length(min=8)])
+
+	confirm_password = PasswordField('Confirm Password',
+		validators=[DataRequired(), Length(min=8), EqualTo('password')])
+	submit = SubmitField('Reset Password')

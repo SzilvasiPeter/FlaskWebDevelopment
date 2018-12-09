9e5041ecc2306ccdb25ccf4f6397d01643fd193a
SzilvasiPeter <peti.szilvasi95@gmail.com>
2018-12-09 12:30:35 +0100

    Add config.py for custom the config of the webapp, Make the enviroment variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,18 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from app.config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '63483d93977177f94771262462df57fb'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'main.index'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+
 mail = Mail(app)
 
 # Import Blueprint
